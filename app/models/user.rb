9cb3ae3554a9d1e9484d7eb620ba4af31aee8424
takahiro-awaji <69838495+takahiro-awaji@users.noreply.github.com>
2020-10-06 14:34:00 +0900

    ユーザーモデル　バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
+  validates :nickname,        presence: true
+  with_options presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/} do
+    validates :first_name
+    validates :last_name
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+  validates :birthday,        presence: true
 end
