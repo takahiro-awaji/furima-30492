ae1fe7a351f1aa53761556a03e3b508dd603cdb4
takahiro-awaji <69838495+takahiro-awaji@users.noreply.github.com>
2020-10-06 18:12:58 +0900

    ユーザー　テストコード作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX
-  validates :nickname,        presence: true
-  with_options presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/} do
+
+  validates :nickname, presence: true
+  validates :password_confirmation, presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
     validates :first_name
     validates :last_name
     validates :first_name_kana
     validates :last_name_kana
   end
-  validates :birthday,        presence: true
+  validates :birthday, presence: true
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,93 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    context '新規登録がうまくいくとき' do
+      it 'ニックネーム、メールアドレス、パスワード、パスワード確認用、名前（姓・名）、名前（カナ）、生年月日がすべて入力されているとき' do
+        expect(@user).to be_valid
+      end
+    end
+
+    context '新規登録がうまくいかないとき' do
+      it 'ニックネームが空のとき' do
+        @user.nickname = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
+      it 'メールアドレスが空のとき' do
+        @user.email = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
+      end
+      it '重複するメールアドレスが存在するとき' do
+        @user.save
+        another_user = FactoryBot.build(:user)
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include('Email has already been taken')
+      end
+      it 'メールアドレスに＠が含まれないとき' do
+        @user.email = 'luffyonepiece.com'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Email is invalid')
+      end
+      it 'パスワードが空のとき' do
+        @user.password = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
+      end
+      it 'パスワードは存在するが、パスワード確認用が空のとき' do
+        @user.password_confirmation = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation can't be blank")
+      end
+      it 'パスワードとパスワード確認用が一致しないとき' do
+        @user.password = '123abc'
+        @user.password_confirmation = '123abb'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+      it 'パスワードが5文字以下のとき' do
+        @user.password = '123ab'
+        @user.password_confirmation = '123ab'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
+      end
+      it 'パスワードが数字or英文字のみのとき' do
+        @user.password = '1234567'
+        @user.password_confirmation = '1234567'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is invalid')
+      end
+      it '名前（姓）が空のとき' do
+        @user.last_name = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name can't be blank")
+      end
+      it '名前（名）が空のとき' do
+        @user.first_name = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name can't be blank")
+      end
+      it '名前（姓）のフリガナが空のとき' do
+        @user.last_name_kana = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name kana can't be blank")
+      end
+      it '名前（名）のフリガナが空のとき' do
+        @user.first_name_kana = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name kana can't be blank")
+      end
+      it '生年月日が空のとき' do
+        @user.birthday = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Birthday can't be blank")
+      end
+    end
+  end
+  
+end
