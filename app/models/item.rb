b8258565e280e5aab6e7b2bbe6bc260ddc6bbad2
takahiro-awaji <69838495+takahiro-awaji@users.noreply.github.com>
2020-10-07 18:19:53 +0900

    価格入力時のバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,9 +8,9 @@ class Item < ApplicationRecord
   validates :shipping_charge_id, presence: true, numericality: { other_than: 1, message: 'Select' }
   validates :shipping_area_id, presence: true, numericality: { other_than: 0, message: 'Select' }
   validates :days_to_ship_id, presence: true, numericality: { other_than: 1, message: 'Select' }
-  validates :price, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'Out of setting range' ,with: /\A[0-9]+\z/, message: 'Half-width number'}
   validates :image, presence: true
-  
+
   belongs_to :user
   has_one_attached :image
 end
