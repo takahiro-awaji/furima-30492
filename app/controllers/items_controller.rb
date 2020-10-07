5ad439d2955f8478b934de10e1d8b57a5c68335d
takahiro-awaji <69838495+takahiro-awaji@users.noreply.github.com>
2020-10-07 16:40:03 +0900

    ActiveHashの導入、バリデーション設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,5 +3,21 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :item_description, :category_id, :item_status_id, :shipping_charge_id, :shipping_area_id, :days_to_ship_id, :price).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  validates :name,   presence: true
+  validates :item_description, presence: true
+  validates :category_id, presence: true, numericality: { other_than: 1, message: 'Select' }
+  validates :item_status_id, presence: true, numericality: { other_than: 1, message: 'Select' }
+  validates :shipping_charge_id, presence: true, numericality: { other_than: 1, message: 'Select' }
+  validates :shipping_area_id, presence: true, numericality: { other_than: 0, message: 'Select' }
+  validates :days_to_ship_id, presence: true, numericality: { other_than: 1, message: 'Select' }
+  validates :price, presence: true
 end
