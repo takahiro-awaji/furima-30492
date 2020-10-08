a69a5310bab10884b0cf42a64bebae7d9b4da5df
takahiro-awaji <69838495+takahiro-awaji@users.noreply.github.com>
2020-10-08 18:23:15 +0900

    商品出品 テストコード作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name                 { 'the_north_face' }
+    item_description     { 'これはテストです！' }
+    category_id          { '2' }
+    item_status_id       { '3' }
+    shipping_charge_id   { '2' }
+    shipping_area_id     { '3' }
+    days_to_ship_id      { '2' }
+    price                { 2000 }
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
